69e05c21964ca8d765cd7c24a4791645a35e4c78
fnakamura <fujiki.nakamura@gmail.com>
2025-09-27 16:01:16 +0900

    add index to 'products' table - Local Final Score: 861 -> 893

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,6 +1,7 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-ALTER TABLE `products` ADD INDEX `idx_name_product_id` (`name`, `product_id`);
 ALTER TABLE `orders` ADD INDEX `idx_user_id` (`user_id`);
 ALTER TABLE `orders` ADD INDEX `idx_product_id` (`product_id`);
 ALTER TABLE `orders` ADD INDEX `idx_shipped_status_created_at` (`shipped_status`, `created_at`);
+ALTER TABLE `products` ADD INDEX `idx_name_product_id` (`name`, `product_id`);
+ALTER TABLE `products` ADD INDEX `idx_shipped_status_product_id_order_id` (`shipped_status`, `product_id`, `order_id`);
 ALTER TABLE `users` ADD INDEX `idx_user_name` (`user_name`);
