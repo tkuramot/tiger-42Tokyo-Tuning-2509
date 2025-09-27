d418f4a10463b333d1aab2e6a5e8bd6af69f7a55
tkuramot <mabdman512@gmail.com>
2025-09-27 15:59:05 +0900

    add index for limited shipping orders

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -3,3 +3,6 @@ ALTER TABLE `products` ADD INDEX `idx_name_product_id` (`name`, `product_id`);
 ALTER TABLE `orders` ADD INDEX `idx_user_id` (`user_id`);
 ALTER TABLE `orders` ADD INDEX `idx_product_id` (`product_id`);
 ALTER TABLE `users` ADD INDEX `idx_user_name` (`user_name`);
+
+ALTER TABLE `orders` ADD INDEX `idx_shipped_product` (`shipped_status`, `product_id`, `created_at`);
+ALTER TABLE `products` ADD INDEX `idx_weight_value` (`weight`, `value` DESC);
