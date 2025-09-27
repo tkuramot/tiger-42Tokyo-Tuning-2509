a8bdf6e78e69a285bcf1d603ebb35a5218c6acc5
fnakamura <fujiki.nakamura@gmail.com>
2025-09-27 13:28:31 +0900

    add index to 'shipped_status'

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -2,3 +2,4 @@
 ALTER TABLE `products` ADD INDEX `idx_name_product_id` (`name`, `product_id`);
 ALTER TABLE `orders` ADD INDEX `idx_user_id` (`user_id`);
 ALTER TABLE `orders` ADD INDEX `idx_product_id` (`product_id`);
+ALTER TABLE `orders` ADD INDEX `idx_shipped_status` (`shipped_status`);
