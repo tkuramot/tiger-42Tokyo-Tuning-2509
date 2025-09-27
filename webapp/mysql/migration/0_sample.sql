81a8e90489507bfb12e2e90040e237d87ad50bbf
tkuramot <mabdman512@gmail.com>
2025-09-27 14:18:02 +0900

    add compound index to orders

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,5 +1,5 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE `products` ADD INDEX `idx_name_product_id` (`name`, `product_id`);
-ALTER TABLE `orders` ADD INDEX `idx_user_id` (`user_id`);
 ALTER TABLE `orders` ADD INDEX `idx_product_id` (`product_id`);
 ALTER TABLE `users` ADD INDEX `idx_user_name` (`user_name`);
+ALTER TABLE `orders` ADD INDEX `idx_user_id_created_at` (`user_id`, `created_at`);
