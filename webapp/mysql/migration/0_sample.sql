b992bc6e1c3d9f3e4cf90bf4afbc1a559bb19692
tkuramot <mabdman512@gmail.com>
2025-09-27 18:53:17 +0900

    add tuika index

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -8,3 +8,6 @@ ALTER TABLE `users` ADD INDEX `idx_user_name` (`user_name`);
 ALTER TABLE `orders` ADD INDEX `idx_shipped_product` (`shipped_status`, `product_id`, `created_at`);
 ALTER TABLE `products` ADD INDEX `idx_weight_value` (`weight`, `value` DESC);
 ALTER TABLE `products` ADD FULLTEXT INDEX `idx_name_desc_fulltext` (`name`, `description`);
+
+ALTER TABLE `user_sessions` ADD INDEX `idx_session_expires` (`session_uuid`, `expires_at`);
+ALTER TABLE `user_sessions` ADD INDEX `idx_user_expires` (`user_id`, `expires_at`);
