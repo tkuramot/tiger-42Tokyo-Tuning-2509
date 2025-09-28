0db03ae0ac017780d5d4d2345fc4cae917fe791c
tkuramot <mabdman512@gmail.com>
2025-09-28 12:57:10 +0900

    Revert "update gitignore"
    This reverts commit e2915f40622c47f5204c1ea461931ee85d3d42ff.

--- webapp/mysql/migration/2_ngram_fulltext_index.sql
+++
@@ -1,5 +0,0 @@
-USE `42Tokyo2508-db`;
-
-ALTER TABLE products DROP INDEX idx_name_desc_fulltext;
-
-ALTER TABLE products ADD FULLTEXT INDEX idx_name_desc_fulltext (name, description) WITH PARSER ngram;
